7cbd826f5e28d353e487439b1c2ac860a6b1bda6
Psami-wondah <okechukwusamuel16@gmail.com>
2025-04-25 15:28:14 +0100

    chore: update pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: "yarn",
       args: "start",
       instances: "max",
-      interpreter: "/bin/bash",
+      interpreter: "/bin/sh",
       exec_mode: "cluster",
       max_memory_restart: "2G",
       env: {
